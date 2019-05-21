ea4a0746b6cd290c45a5e9a68a5f682c711e5170
pierce <pierceshih@gmail.com>
2019-05-21 10:48:03 +0800

    Add home and user Routers and views for register and login page.

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const exphbs = require('express-handlebars');
+
+const UserRouter = require('./routers/user');
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
 app.get('/', (req, res) => {
   res.send('首頁');
 })
+
+app.use('/users', UserRouter);
 
 app.listen(port, () => {
   console.log('Express is running.');
